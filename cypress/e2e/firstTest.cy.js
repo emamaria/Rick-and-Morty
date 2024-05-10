09d5b57b45aef5643e52cf8fc08808d5c77dfd4c
emamaria <emascript@gmail.com>
2024-05-10 20:06:38 +0200

    following pages test part 1

--- cypress/e2e/firstTest.cy.js
+++ cypress/e2e/firstTest.cy.js
@@ -1,4 +1,4 @@
-describe('Rick and Morty Page', () => {
+describe('Rick and Morty First Page Test', () => {
 
   
   beforeEach(() => {
@@ -22,21 +22,67 @@ describe('Rick and Morty Page', () => {
     cy.get('.character-container').find('p').should('have.length', 20)
  })
 
-  it('rendered images each container id must match with numbers from 1 to 20', () =>{
+  it('first and last container id of rendered images must match with 1 and 20', () =>{
  
-      for(let i = 1; i <= 20; i++){
+       cy.get('.character-container').then(containers => {
 
+        let lastContainerIndex = containers.length - 1
+        cy.wrap(containers).eq(0).invoke('attr', 'id').should('contain', 1)
+        cy.wrap(containers).eq(lastContainerIndex).invoke('attr', 'id').should('contain', 20)
 
-        cy.get('.character-container').invoke('attr', 'id').each(id => {
-      
-        
-         
        })
-      }
-
+     
       
-    
+
+    })
+       
+
   })
 
+  describe.only('Rick and Morty rest of pages testing', () => {
 
-})
+
+         
+     beforeEach(() => {
+         cy.visit('http://localhost:5173/')
+      })
+
+
+     it('Shows Prev button as well once click Next button of / page',() => {
+        cy.contains('Next').click()
+        cy.contains('Prev').should('exist')
+     })
+
+     it('shows 20 images after every click in Next button until this button dissapears', () => {
+           
+      let buttonsNumbers;
+      cy.contains('Next').click()
+
+       cy.get('button').then( buttons => {
+        buttonsNumbers = cy.wrap(buttons).length
+         while(buttonsNumbers === 2){
+          cy.contains('Next').click()
+          cy.wait(1000)
+          cy.get('img').should('have.length', 20)
+          buttonsNumbers = cy.get('buttons').length
+          console.log("ema", buttonsNumbers)
+          cy.contains('Next').click()
+         }
+       })
+        
+        //  const buttonsNumbers =  cy.get('button').length
+      //  const NextButton = cy.contains('Next').should('exist')
+      //  console.log(NextButton)
+       
+      //  while(cy.contains('Next')){
+      //      console.log("ema")
+      //  }
+      //   if(NextButton){
+      //   NextButton.click()
+      //   cy.get('img').should('have.length', 20)
+      // }
+       
+     })
+      
+
+  })
